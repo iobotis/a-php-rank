7ac7a256ff03f7868fc041cd8c0bf8eeb774bac7
iobotis <iobotis4@gmail.com>
2016-02-08 00:04:09 +0200

    Ranking interface created.

---
+++ RankingAlgorithmInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jb
+ */
+interface RankingAlgorithmInterface {
+    
+    public function isReady();
+
+    public function run();
+}
+
+?>
